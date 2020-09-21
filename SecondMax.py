bd0dd1350825a52b2317c8ef964603b043e52ab3
Alexander Balashov <alaex777@gmail.com>
2020-09-21 23:38:14 +0300

    Add SecondMax

--- SecondMax.py
+++ SecondMax.py
@@ -1,15 +1,20 @@
 a = int(input())
 max1 = a
 a = int(input())
-max2 = min(a, max1)
-while a != 0:
-	if a > max1:
-		max2 = max1
-		max1 = a
-	elif a > max2:
-		max2 = a
-	a = int(input())
-if max2 == max1:
-	print("NO")
+if a != 0:	
+	max2 = min(a, max1)
+	while a != 0:
+		if a > max1:
+			max2 = max1
+			max1 = a
+		elif a > max2:
+			max2 = a
+		if max2 == max1 :
+			max2 = a
+		a = int(input())
+	if max2 == max1:
+		print("NO")
+	else:
+		print(max2)
 else:
-	print(max2)
+	print("NO")
